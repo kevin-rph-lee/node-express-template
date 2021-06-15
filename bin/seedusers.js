ed3bf5a07518b6d06f5973ec94c682813fe0ccbc
Kevin <kevin.rph.lee@gmail.com>
2021-06-15 16:47:11 -0700

    updated seed users to use heroku

--- bin/seedusers.js
+++ bin/seedusers.js
@@ -1,31 +1,85 @@
 const { Pool } = require('pg');
+const Client = require('pg-native');
 
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 
-const dbParams = require('../lib/db.js');
-const db = new Pool(dbParams);
+if(process.env.DATABASE_URL){
+  const { Client } = require('pg');
 
-db.connect();
+  const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
-let queryStringInsertSeeds = `INSERT INTO users(
+  db.connect();
+
+
+  let queryStringInsertSeeds = `INSERT INTO users(
     username, password, role) 
     VALUES($1, $2, 1),
     ($3, $4, 2),
     ($5, $6, 2),
     ($7, $8, 2);`
-let valuesInsertSeeds =  ['admin@admin.com', bcrypt.hashSync(process.env.ADMIN_PASS, 10), 
-'user1@user.com', bcrypt.hashSync(process.env.USER1_PASS, 10),
-'user2@user.com', bcrypt.hashSync(process.env.USER2_PASS, 10),
-'user3@user.com', bcrypt.hashSync(process.env.USER3_PASS, 10)]
+  let valuesInsertSeeds =  ['admin@admin.com', bcrypt.hashSync(process.env.ADMIN_PASS, 10), 
+  'user1@user.com', bcrypt.hashSync(process.env.USER1_PASS, 10),
+  'user2@user.com', bcrypt.hashSync(process.env.USER2_PASS, 10),
+  'user3@user.com', bcrypt.hashSync(process.env.USER3_PASS, 10)]
 
-db.query(queryStringInsertSeeds, valuesInsertSeeds)
-.then(data => {
-    console.log('Done!')
-    db.end();
-    return;
-})
-.catch(err => {
-  console.log(err)
-});
+  db.query(queryStringInsertSeeds, valuesInsertSeeds)
+  .then(data => {
+      console.log('Done!')
+      db.end();
+      return;
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+
+
+
+
+
+
+  // client.query('SELECT * FROM users;', (err, res) => {
+  //   if (err) throw err;
+  //   for (let row of res.rows) {
+  //     console.log(JSON.stringify(row));
+  //   }
+  //   client.end();
+  // });
+
+} else {
+
+  console.log('Non heroku Environment detected')
+  const dbParams = require('../lib/db.js');
+  const db = new Pool(dbParams);
+
+  db.connect();
+
+  let queryStringInsertSeeds = `INSERT INTO users(
+      username, password, role) 
+      VALUES($1, $2, 1),
+      ($3, $4, 2),
+      ($5, $6, 2),
+      ($7, $8, 2);`
+  let valuesInsertSeeds =  ['admin@admin.com', bcrypt.hashSync(process.env.ADMIN_PASS, 10), 
+  'user1@user.com', bcrypt.hashSync(process.env.USER1_PASS, 10),
+  'user2@user.com', bcrypt.hashSync(process.env.USER2_PASS, 10),
+  'user3@user.com', bcrypt.hashSync(process.env.USER3_PASS, 10)]
+
+  db.query(queryStringInsertSeeds, valuesInsertSeeds)
+  .then(data => {
+      console.log('Done!')
+      db.end();
+      return;
+  })
+  .catch(err => {
+    console.log(err)
+  });
+}
+
